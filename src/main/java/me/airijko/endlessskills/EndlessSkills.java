436d0f21e815d3da1850bdc2e77b9e8c45856d82
Airijko <airijkoblade@gmail.com>
2024-03-05 22:27:37 -0600

    first commit

---
+++ src/main/java/me/airijko/endlessskills/EndlessSkills.java
@@ -0,0 +1,17 @@
+package me.airijko.endlessskills;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EndlessSkills extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
